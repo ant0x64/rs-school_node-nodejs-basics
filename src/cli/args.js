857d243278328bb567c5f0a2cc08c42b04198ceb
Anton D <adkv.lab@gmail.com>
2024-01-24 11:32:09 +0000

    feat: cli/args.js

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,19 @@
+'use strict';
+
+import process from 'process';
+
 const parseArgs = () => {
-    // Write your code here 
+    const argvArray = process.argv.slice(2);
+    const outputArray = [];
+
+    for(let i = 0; i < argvArray.length; i++) {
+        if(argvArray[i].startsWith('--') && argvArray[i + 1]) {
+            outputArray.push(`${argvArray[i].substring(2)} is ${argvArray[i + 1]}`);
+            i++;
+        }
+    }
+
+    console.log(outputArray.join(', '));
 };
 
 parseArgs();
