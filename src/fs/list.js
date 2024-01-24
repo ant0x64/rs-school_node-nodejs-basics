3d06c6d4dbd91aee1ebbc859cd577776a7c2f52e
Anton D <adkv.lab@gmail.com>
2024-01-24 09:57:52 +0000

    feat: fs/list.js

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,24 @@
+'use strict';
+
+import { readdir } from 'fs/promises';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+
 const list = async () => {
-    // Write your code here 
+    const exec_dir = dirname(fileURLToPath(import.meta.url));
+    const files_dir = join(exec_dir, 'files');
+
+    try {
+        (await readdir(files_dir, { recursive: true })).map(name => {
+            console.log(name);
+        });
+    } catch (err) {
+        if (err.code === 'ENOENT') {
+            throw new Error('FS operation failed');
+        } else {
+            throw err;
+        }
+    }
 };
 
 await list();
