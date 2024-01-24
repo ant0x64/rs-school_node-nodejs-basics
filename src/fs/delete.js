7346ca7bcca33d3a3ec5d9694ca41713534b4e2e
Anton D <adkv.lab@gmail.com>
2024-01-24 09:22:30 +0000

    feat: fs/delete.js

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,23 @@
+'use strict';
+
+import { unlink, access } from 'fs/promises';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+
 const remove = async () => {
-    // Write your code here 
+    const exec_dir = dirname(fileURLToPath(import.meta.url));
+    const file_path = join(exec_dir, 'files', 'fileToRemove.txt');
+
+    try {
+        await access(file_path);
+    } catch (err) {
+        if(err.code === 'ENOENT') {
+            throw new Error('FS operation failed');
+        }
+        throw err;
+    }
+
+    unlink(file_path);
 };
 
 await remove();
