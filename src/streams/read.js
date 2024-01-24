dc0fc3a4350ba0a715cdc9f6dd4345454928d31c
Anton D <adkv.lab@gmail.com>
2024-01-24 15:19:20 +0000

    feat: streams/read.js

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,14 @@
+'use strict';
+
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { pipeline } from 'stream/promises';
+import { createReadStream } from 'fs';
+import process from 'process';
+
 const read = async () => {
-    // Write your code here 
+    const file_path = join(dirname(fileURLToPath(import.meta.url)), 'files', 'fileToRead.txt');
+    await pipeline(createReadStream(file_path), process.stdout);
 };
 
 await read();
