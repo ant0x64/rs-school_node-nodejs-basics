f94fcc7b4e1f4ac52e6ee9b74683951eb31e03ad
Anton D <adkv.lab@gmail.com>
2024-01-24 16:13:41 +0000

    feat: zip/compress.js

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,18 @@
+'use strict';
+
+import { createGzip } from 'zlib';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { pipeline } from 'stream/promises';
+import { createReadStream, createWriteStream } from 'fs';
+
 const compress = async () => {
-    // Write your code here 
+    const dir_name = join(dirname(fileURLToPath(import.meta.url)), 'files');
+
+    const file_path = join(dir_name, 'fileToCompress.txt');
+    const gzip_path = join(dir_name, 'archive.gz');
+
+    pipeline(createReadStream(file_path), createGzip(), createWriteStream(gzip_path));
 };
 
 await compress();
