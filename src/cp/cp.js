6869822b71f561452cccdc515ac66660bad6e7e9
Anton D <adkv.lab@gmail.com>
2024-01-27 09:35:41 +0000

    feat: cp

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,13 @@
-const spawnChildProcess = async (args) => {
-    // Write your code here
+'use strict';
+
+import { fork } from 'child_process';
+import { fileURLToPath } from 'url';
+import { join, dirname } from 'path';
+
+const process_path = join(dirname(fileURLToPath(import.meta.url)), 'files', 'script.js');
+
+const spawnChildProcess = async (args = ['default argument']) => {
+    fork(process_path, args);
 };
 
 // Put your arguments in function call to test this functionality
