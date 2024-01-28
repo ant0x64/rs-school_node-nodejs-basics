3ce1edc281d61cbea87fae7746cf51d1311a749f
Anton D <adkv.lab@gmail.com>
2024-01-28 15:48:32 +0100

    fix: implement cp IPC communication

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -7,7 +7,20 @@ import { join, dirname } from 'path';
 const process_path = join(dirname(fileURLToPath(import.meta.url)), 'files', 'script.js');
 
 const spawnChildProcess = async (args = ['default argument']) => {
-    fork(process_path, args);
+    const child = fork(process_path, args, {silent: true});
+
+    const sender = (data) => {
+        child.send(data.toString());
+    };
+
+    process.stdin.on('data', sender);
+    child.stdout.pipe(process.stdout);
+
+    child.on('message', (data) => console.log(data.trim()));
+    child.on('disconnect', (code) => {
+        process.stdin.removeListener('data', sender);
+        process.exit(code);
+    })
 };
 
 // Put your arguments in function call to test this functionality
